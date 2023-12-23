334309d5aee9d8c35632ce62ab918fd91fd2a84f
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-23 19:06:15 +0900

    add : UI 생성 > 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import {createGlobalStyle} from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,7 +14,11 @@ function App() {
     return (
         <>
             <GlobalStyle/>
-            <div>안녕하세요!</div>
+            <TodoTemplate>
+                <TodoHead/>
+                <TodoList/>
+                <TodoCreate/>
+            </TodoTemplate>
         </>
     );
 }
